be4fef6b581eac76f613622d34fa46e67993dda4
Stephen Chen <stephen.x.chen@oracle.com>
2015-09-21 09:36:12 -0400

    Adding static files

--- magicaljourney/settings.py
+++ magicaljourney/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'magicaljourney.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'leaguebuddy/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,5 +100,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATIC_URL = '/static/'
--- magicaljourney/urls.py
+++ magicaljourney/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
+import settings
 import leaguebuddy.views
 
 urlpatterns = [
     url(r'^$', leaguebuddy.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
